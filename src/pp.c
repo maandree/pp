df37dccb4bf73d765a374d8baf84b713484dffb3
Mattias Andrée <maandree@operamail.com>
2015-12-01 02:51:25 +0100

    printing loop
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/pp.c
+++ src/pp.c
@@ -28,6 +28,8 @@
 #include <errno.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/ioctl.h>
 #include <sys/stat.h>
 
 #define  t(...)  do { if (__VA_ARGS__) goto fail; } while (0)
@@ -55,24 +57,72 @@ struct page {
 /**
  * The name of the process.
  */
-const char *argv0;
+static const char *argv0;
 
 /**
  * The index (0-based) of the current page.
  */
-size_t current_page = 0;
+static size_t current_page = 0;
 
 /**
  * The number of pages.
  */
-size_t page_count = 0;
+static size_t page_count = 0;
 
 /**
  * All loaded pages. Refer to `page_count`
  * for the number of contained pages.
  */
-struct page *pages;
+static struct page *pages;
 
+/**
+ * Have the terminal been resized?
+ */
+static volatile sig_atomic_t caught_sigwinch = 1;
+
+/**
+ * The width of the terminal.
+ */
+static size_t width = 72;
+
+/**
+ * The height of the terminal.
+ */
+static size_t height = 56;
+
+
+
+
+/**
+ * Signal handler for SIGWINCH.
+ * Invoked when the terminal resizes.
+ */
+static void sigwinch(int signo)
+{
+  signal(signo, sigwinch);
+  caught_sigwinch = 1;
+}
+
+
+/**
+ * Get the size of the terminal.
+ */
+static void get_terminal_size(void)
+{
+	struct winsize winsize;
+
+	if (!caught_sigwinch)
+		return;
+
+	caught_sigwinch = 0;
+
+	while (ioctl(STDOUT_FILENO, (unsigned long)TIOCGWINSZ, &winsize) < 0)
+		if (errno != EINTR)
+			return;
+
+	height = winsize.ws_row;
+	width = winsize.ws_col;
+}
 
 
 /**
@@ -85,26 +135,149 @@ struct page *pages;
  */
 static int display_page(int bar, int page)
 {
-	ssize_t wrote;
+	ssize_t n;
+	size_t stop;
 	struct page p;
 
 	p = pages[current_page];
 	while (p.size) {
-		wrote = write(STDOUT_FILENO, p.content, p.size);
-		if (wrote < 0) {
+		n = write(STDOUT_FILENO, p.content, p.size);
+		if (n < 0) {
 			if (errno == EINTR)
 				continue;
 			return -1;
 		}
-		p.content += wrote;
-		p.size -= (size_t)wrote;
+		p.content += n;
+		p.size -= (size_t)n;
 	}
 
-	/* TODO */ (void) bar, (void) page;
+	if ((bar | page) == 0)
+		return 0;
+
+	get_terminal_size();
+
+	t (snprintf(NULL, 0, "%zu (%zu)%zn", current_page, page_count, &n) < 0);
+	if (bar && current_page) {
+		t (fprintf(stdout, "\033[%zu;1H\033[0;7m\033[%zu@\033[27m",
+			   height, stop = current_page * width / (page_count - 1)) < 0);
+	} else if (page) {
+		t (fprintf(stdout, "\033[%zu;%zuH\033[0m%zu (%zu)",
+			   height, width - 1 - (size_t)n, current_page, page_count) < 0);
+	}
+	else if (bar && page && current_page) {
+		char buffer[2 * 3 * sizeof(size_t) + sizeof(" ()\033[27m")];
+		sprintf(buffer, "%zu (%zu)", current_page, page_count);
+		if (width - 1 - (size_t)n < stop)
+			stop = 0;
+		else
+			stop -= width - 1 - (size_t)n;
+		if (stop > (size_t)n)
+			stop = (size_t)n;
+		memmove(buffer + stop + sizeof("\033[27m") - 1, buffer + stop, strlen(buffer + stop));
+		memcpy(buffer + stop, "\033[27m", sizeof("\033[27m") - 1);
+		t (fprintf(stdout, "\033[%zu;%zuH\033[0;7m%s",
+			   height, width - 1 - (size_t)n, buffer) < 0);
+	}
+
+	t (fflush(stdout));
 
 	return 0;
+fail:
+	return -1;
 }
 
+
+/**
+ * Interactively display the file.
+ * 
+ * @param   fd    File descriptor for reading from the terminal.
+ * @param   bar   Print progress bar?
+ * @param   page  Print page number and page count?
+ * @return        0 on success, -1 on error.
+ */
+static int display_file(int fd, int bar, int page)
+{
+	ssize_t n;
+	char c;
+	int state = 0;
+	int command = 0;
+
+next:
+	t (fprintf(stdout, "\033[H\033[2J") < 0);
+	t (fflush(stdout));
+	t (display_page(bar, page));
+
+	for (command = 99; command == 99;) {
+		n = read(fd, &c, sizeof(c));
+		if (n == 0) {
+			return 0;
+		} else if (n < 0) {
+			t (errno != EINTR);
+			continue;
+		}
+
+		if (state == 1) { /* ESC */
+			state = (c == '[' ? 2 : 0);
+		} else if (state == 2) { /* CSI */
+			if (strchr("AC", c)) /* up or right */
+				command = -1;
+			else if (strchr("BD", c)) /* down or left */
+				command = +1;
+			else if (c == '5') /* CSI 5 */
+				state = 5;
+			else if (c == '6') /* CSI 6 */
+				state = 6;
+			else
+				state = 0;
+		} else if ((state == 5) || (state == 6)) { /* CSI 5 or CSI 6*/
+			if (c == '~')
+				command = (state == 5 ? -1 : +1); /* page up or page down*/
+			else
+				state = 0;
+		} else if (c == '\033') { /* ESC*/
+			state = 1;
+		} else if (c == 'L' - '@') { /* C-l */
+			command = 0;
+		} else if (c == 'q') { /* q */
+			return 0;
+		}
+	}
+
+	/*
+	 * Up   = \e[A
+	 * Down = \e[B
+	 * 
+	 * Page Up   = \e[5~
+	 * Page Down = \e[6~
+	 * 
+	 * Right = \e[C
+	 * Left  = \e[D
+	 */
+
+	if ((command == -1) && current_page) {
+		current_page -= 1;
+	} else if ((command == +1) && (current_page + 1 < page_count)) {
+		current_page += 1;
+	}
+
+	goto next;
+fail:
+	return -1;
+}
+
+
+/**
+ * Load pages.
+ * 
+ * @param   fd          File descriptor for reading the file.
+ * @param   keep_empty  Display empty pages?
+ * @return              0 on success, -1 on error.
+ */
+static int load_pages(int fd, int keep_empty)
+{
+	/* TODO */ (void) fd, (void) keep_empty;
+	return 0;
+}
 
 
 /**
@@ -181,7 +354,8 @@ int main(int argc, char *argv[])
 		t (fd == -1);
 	}
 
-	/* TOOD Load pages. */
+	/* Load pages. */
+	t (load_pages(fd, f_empty));
 
 	/* We do not need the file anymore. */
 	close(fd), fd = -1;
@@ -203,7 +377,9 @@ int main(int argc, char *argv[])
 	fd = open("/dev/tty", O_RDONLY);
 	t (fd == -1);
 
-	/* TODO Display file. */
+	/* Display file. */
+	signal(SIGWINCH, sigwinch);
+	t (display_file(fd, f_bar, f_page));
 
 	/* We do not need the input from the terminal anymore. */
 	close(fd), fd = -1;
