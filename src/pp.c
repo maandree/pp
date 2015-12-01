3f599ac99f3669f55fc9c14f2e41c25f17289111
Mattias Andrée <maandree@operamail.com>
2015-12-01 03:57:39 +0100

    whitespace
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/pp.c
+++ src/pp.c
@@ -158,10 +158,10 @@ static int display_page(int bar, int page)
 	t (snprintf(NULL, 0, "%zu (%zu)%zn", current_page + 1, page_count, &n) < 0);
 	if (bar && current_page) {
 		t (fprintf(stdout, "\033[%zu;1H\033[0;7m\033[%zu@\033[27m",
-			   height, stop = current_page * width / (page_count - 1)) < 0);
+		           height, stop = current_page * width / (page_count - 1)) < 0);
 	} else if (page) {
 		t (fprintf(stdout, "\033[%zu;%zuH\033[0m%zu (%zu)",
-			   height, width - 1 - (size_t)n, current_page + 1, page_count) < 0);
+		           height, width - 1 - (size_t)n, current_page + 1, page_count) < 0);
 	}
 	else if (bar && page && current_page) {
 		char buffer[2 * 3 * sizeof(size_t) + sizeof(" ()\033[27m")];
@@ -175,7 +175,7 @@ static int display_page(int bar, int page)
 		memmove(buffer + stop + sizeof("\033[27m") - 1, buffer + stop, strlen(buffer + stop));
 		memcpy(buffer + stop, "\033[27m", sizeof("\033[27m") - 1);
 		t (fprintf(stdout, "\033[%zu;%zuH\033[0;7m%s",
-			   height, width - 1 - (size_t)n, buffer) < 0);
+		           height, width - 1 - (size_t)n, buffer) < 0);
 	}
 
 	t (fflush(stdout));
